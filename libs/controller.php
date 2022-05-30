58908fe4444fa62647aee9ee5b509b127713f702
Pablo Bruno <hola@pablobrunodw.com>
2022-05-29 22:51:45 -0300

    NEW: nuevas funciones de controladores

--- libs/controller.php
+++ libs/controller.php
@@ -13,4 +13,41 @@ class Controller
             $this->model = new $modelName;
         }
     }
+
+    public function existPOST($params)
+    {
+        foreach ($params as $param) {
+            if(!isset($_POST[$param])){
+                error_log('Controller::existPOST->No existe el parametro ' . $param);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function existGET($params)
+    {
+        foreach ($params as $param) {
+            if(!isset($_GET[$param])){
+                error_log('Controller::existGET->No existe el parametro ' . $param);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function getGET($val)
+    {
+        return $_GET[$val];
+    }
+
+    public function getPOST($val)
+    {
+        return $_POST[$val];
+    }
+
+    public function redirect($url,$mensajes)
+    {
+        
+    }
 }
